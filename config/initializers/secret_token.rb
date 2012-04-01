16c4ed3d4d2a34e72e9a9748b3ca901eee247615
Gareth Adams <gareth@Gareths-MacBook-Pro.local>
2012-04-01 13:25:56 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Watercoolr::Application.config.secret_token = 'c7147cbae0b162000df081b5b182c35148db7006dcd4dbd631530a82a26921271cdc730555f750096e748e4941a859ee1cedaee27568d28507cf43516276c05f'
