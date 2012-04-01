c70ed89ed88c93cfa5009ed75942ca5c4cb42a4b
Ian Ballantyne <ianballantyne@turbulenz.biz>
2012-04-01 14:26:41 +0100

    Added a basic soundcloud widget

---
+++ app/assets/javascripts/media.js
@@ -0,0 +1,97 @@
+
+(function(){
+
+  var host2widgetBaseUrl = {
+    "wt.soundcloud.dev" : "wt.soundcloud.dev/",
+    "wt.soundcloud.com" : "wt.soundcloud.com/player/",
+    "w.soundcloud.com"  : "w.soundcloud.com/player/"
+  };
+
+  var consoleBox = document.querySelector('.console');
+
+  var forEach = Array.prototype.forEach;
+
+  function addEvent(element, eventName, callback) {
+    if (element.addEventListener) {
+      element.addEventListener(eventName, callback, false);
+    } else {
+      element.attachEvent(eventName, callback, false);
+    }
+  }
+
+  function updateConsole(value) {
+    consoleBox.value = value +"\n" + consoleBox.value;
+  }
+
+  var widgetUrl = "http://api.soundcloud.com/users/1539950/favorites";
+
+  consoleBox.value = "Loading...";
+
+  var iframe = document.querySelector('.iframe');
+  iframe.src = location.protocol + "//" + host2widgetBaseUrl[location.host] + "?url=" + widgetUrl;
+  iframe.onload = function() {
+
+    var widget = SC.Widget(iframe);
+    widget.bind(SC.Widget.Events.READY, function() {
+      var actionButtons = document.querySelectorAll('.actionButtons button');
+      forEach.call(actionButtons, function(button) {
+        addEvent(button, 'click', function(e) {
+          if (e.target !== this) {
+            e.stopPropagation();
+            return false;
+          }
+          var input = this.querySelector('input');
+          var value = input && input.value;
+          widget[this.className](value);
+        });
+      });
+
+      var getterButtons = document.querySelectorAll('.getterButtons button');
+      forEach.call(getterButtons, function(button){
+        addEvent(button, 'click', function(e) {
+          widget[this.className](function(value){
+            updateConsole(button.getAttribute('caption') + " " + JSON.stringify(value));
+          });
+        });
+      });
+
+      var eventKey, eventName;
+      for (eventKey in SC.Widget.Events) {
+        (function(eventName, eventKey) {
+          eventName = SC.Widget.Events[eventKey];
+          widget.bind(eventName, function(eventData) {
+            updateConsole("SC.Widget.Events." + eventKey +  " " + JSON.stringify(eventData || {}));
+          });
+        }(eventName, eventKey))
+      }
+
+      var widgetLinks = document.querySelectorAll('.widgetLinks a');
+      var widgetUrlInput = document.querySelector('.urlInput');
+      forEach.call(widgetLinks, function(link) {
+        addEvent(link, 'click', function(e) {
+          widgetUrlInput.value = this.getAttribute("href");
+          e.preventDefault();
+        });
+      });
+
+      var reloadButton = document.querySelector('.reload');
+      addEvent(reloadButton, 'click', function() {
+        var widgetOptions = getWidgetOptions();
+        widgetOptions.callback = function(){
+          updateConsole('Widget is reloaded.')
+        };
+        widget.load(widgetUrlInput.value, widgetOptions);
+      });
+
+      function getWidgetOptions() {
+        var optionInputs = document.querySelectorAll('.widgetOptions input');
+        var widgetOptions = {};
+        forEach.call(optionInputs, function(option){
+          widgetOptions[option.id] = option.type === 'text' ? option.value : option.checked;
+        });
+        return widgetOptions;
+      }
+    });
+  };
+
+}());
